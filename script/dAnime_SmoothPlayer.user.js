5d6de376bf4238e647420128c7027f000e1f851e
chimaha <107383950+chimaha@users.noreply.github.com>
2024-02-10 06:30:01 +0900

    再生位置に戻るまで再生を停止する

--- script/dAnime_SmoothPlayer.user.js
+++ script/dAnime_SmoothPlayer.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/chimaha/dAnimeSmoothPlayer
 // @match       https://animestore.docomo.ne.jp/animestore/sc_d_pc*
 // @grant       none
-// @version     1.1
+// @version     1.4
 // @author      chimaha
 // @description dアニメストアのFirefox限定フリーズバグを擬似的に回避します 
 // @license     MIT license
@@ -18,40 +18,55 @@
 
 const video = document.querySelector('video');
 function freezeRemover(setTime) {
-    console.log(setTime);
     const observer = new MutationObserver(() => {
         observer.disconnect();
         let time = video.currentTime;
-        video.currentTime = time - 5;
+        video.currentTime = time - 3;
+        video.pause();
         setTimeout(() => {
             video.currentTime = time;
+            video.play();
         }, setTime);
     });
     const config = { childList: true };
     observer.observe(document.querySelector("#time"), config);
 }
 
+
 // 動画を開いた時
-freezeRemover(250);
+freezeRemover(300);
 
 // シークバーをクリックした時
 const seekbar = document.querySelector('.seekArea');
 seekbar.addEventListener("mouseup", () => {
-    console.log("クリック");
     freezeRemover(400);
 });
 
 // シークバー下の30秒戻るボタンを押した時
 const backButton = document.querySelector('.buttonArea > .back');
 backButton.addEventListener("click", () => {
-    console.log("クリック");
     freezeRemover(400);
 });
 
 // 左矢印キーとjキーを押した時
 document.addEventListener("keyup", e => {
     if (e.key == "ArrowLeft" || e.key == "j") {
-        console.log(e.key);
-        freezeRemover(480)
+        freezeRemover(400);
     }
 });
+
+// 10秒以上一時停止した場合、再生再開時に実行
+let stopTime;
+let startTIme;
+video.addEventListener("pause", () => {
+    stopTime = new Date();
+})
+video.addEventListener("play", () => {
+    if (stopTime) {
+        startTIme = new Date();
+        const sec = (startTIme.getTime() - stopTime.getTime()) / 1000;
+        if (sec > 10) {
+            freezeRemover(0);
+        }
+    }
+})
